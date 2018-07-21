05d0815b95c5fddd73366405858af847c6998bcc
Howard Kam <hkam@roosevelths.k12.hi.us>
2018-07-20 17:47:57 -1000

    Sample 2

---
+++ Part 2 sample - V-bind/script2sample.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: "hello world",
+      title: "You loaded the page on " + new Date(),
+      url: "https://blog.tuleap.org/sites/default/files/logo-vue-js.png"
+    }
+  })
--- Sample 1 - Checked shopping list/store.js
+++ Sample 1 - Checked shopping list/store.js
@@ -10,8 +10,8 @@ var item = new Vue({
   data: {
     goods: [
       {qty: 10, desc: "Laptops", price: 799.95},
-      {qty: 5, desc: "HDMI Cables"},
-      {qty: 7, desc: "Monitors"}
+      {qty: 5, desc: "HDMI Cables", price: 10.99},
+      {qty: 7, desc: "Monitors", price: 119.99}
     ] 
   }
 })
